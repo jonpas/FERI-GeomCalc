0d872255dd46d2f5d394949743f65c312599c5b0
jonpas <jonpas33@gmail.com>
2018-03-28 18:51:51 +0200

    Add line plotting between points

--- geomcalc.py
+++ geomcalc.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from matplotlib.patches import Rectangle
+from matplotlib.patches import Rectangle, ConnectionPatch
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
@@ -14,6 +14,9 @@ from modes import points_lines as pl
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.txt_points = []  # List of tuples (elements)
+        self.lines = []  # List of tuples (points)
 
         self.initUI()
 
@@ -48,7 +51,6 @@ class MainWindow(QWidget):
         tab_pl.layout.addWidget(self.cb_type)
         tab_pl.layout.addStretch()
 
-        self.txt_points = []  # List of tuples (elements)
         for i in range(1, 5):
             lbl_p = QLabel("P{}:".format(i))
             txt_px = QLineEdit()
@@ -98,30 +100,55 @@ class MainWindow(QWidget):
     def on_plot_click(self, event):
         if event.xdata is not None and event.ydata is not None:
             if self.tabs.currentIndex() == 0:
-                if len(self.plot.patches) > self.pl.mode + 1:
+                npatches = len(self.plot_get_points())
+                if npatches > self.pl.mode + 1:
                     self.plot_clear()
-                self.plot_point(event.xdata, event.ydata)
+                self.plot_point((event.xdata, event.ydata))
+                if self.pl.mode == 1 and npatches == 2:
+                    self.plot_line(self.pl.p2, (event.xdata, event.ydata))
+                elif self.pl.mode == 2:
+                    print(npatches)
+                    if npatches == 1:
+                        self.plot_line(self.pl.p1, (event.xdata, event.ydata))
+                    elif npatches == 3:
+                        self.plot_line(self.pl.p3, (event.xdata, event.ydata))
                 self.pl_update_ui(self.pl, self.txt_points, replot=False)
 
     def on_plot_resize(self, event):
         self.plot.set_xlim((0, event.width))
         self.plot.set_ylim((0, event.height))
 
+    def plot_get_points(self):
+        return [p for p in self.plot.patches if isinstance(p, Rectangle)]
+
+    def plot_get_lines(self):
+        return [p for p in self.plot.patches if isinstance(p, ConnectionPatch)]
+
     def plot_clear(self):
         [patch.remove() for patch in self.plot.patches[::-1]]
         [text.remove() for text in self.plot.texts[::-1]]
+        self.lines = []
 
-    def plot_point(self, x, y, text="", color="black"):
-        rect = Rectangle((x, y), 5, 5, color=color)
+    def plot_point(self, p, text="", color="black"):
+        x, y = p
+        rect = Rectangle((x - 2.5, y - 2.5), 5, 5, color=color)
         self.plot.add_patch(rect)
 
-        npatches = len(self.plot.patches)
+        npatches = len(self.plot_get_points())
         text = "P{}".format(npatches) if not text else text
         self.plot.text(x + 7, y - 5, text, fontsize=9, color=color)
         self.figure.canvas.draw()
 
         self.txt_points[npatches - 1][0].setText(str(int(x)))
         self.txt_points[npatches - 1][1].setText(str(int(y)))
+
+    def plot_line(self, p1, p2, color="black", temp=False):
+        line = ConnectionPatch(p1, p2, coordsA="data", coordsB="data", color=color)
+        self.plot.add_patch(line)
+        self.figure.canvas.draw()
+
+        if not temp:
+            self.lines.append((p1, p2))
 
     def pl_update_ui(self, pl, txt_points, replot=False):
         # Toggle available points based on mode
@@ -144,9 +171,10 @@ class MainWindow(QWidget):
 
         if replot:
             # (Re)plot all points
+            lines = self.lines
             self.plot_clear()
-            for point in points[:self.pl.mode + 2]:
-                self.plot_point(point[0], point[1])
+            [self.plot_point(point) for point in points[:self.pl.mode + 2]]
+            [self.plot_line(line[0], line[1]) for line in lines]
 
     def pl_calculate(self):
         self.pl_update_ui(self.pl, self.txt_points, replot=True)
@@ -158,11 +186,11 @@ class MainWindow(QWidget):
         result, text, pp, closest = self.pl.calculate()
 
         if pp is not None:
-            self.plot_point(pp[0], pp[1], text="PP", color="red")
+            self.plot_point(pp, text="PP", color="red")
         if np.array_equal(closest, pp):
-            print("TODO plot line from p1 to pp")
+            self.plot_line(self.pl.p1, pp, temp=True)
         else:
-            print("TODO plot line from p1 to closest")
+            self.plot_line(self.pl.p1, closest, temp=True)
 
         msg.setText(text)
         msg.exec()
